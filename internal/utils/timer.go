30081ea5f921ff00443e497606b12870abedf6db
mehkij <disismehki@gmail.com>
2025-05-06 10:17:41 -0400

    feat: create new timer utility function

---
+++ internal/utils/timer.go
@@ -0,0 +1,65 @@
+package utils
+
+import (
+	"time"
+)
+
+/*
+Timer takes several parameters:
+
+A duration (in seconds);
+A stop signal;
+A callback function to be called every tick;
+A callback function that is called when the timer ends;
+And a final callback function that is called whenever the stop signal recieves a "true" signal.
+*/
+func Timer(duration int, stop <-chan bool, callback func(duration int), end func(), interrupt func()) {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+	// sleep := time.Duration(duration + 1) // Counts 0 as the last number
+
+	timeLeft := duration
+
+	// go func() {
+	// 	for {
+	// 		select {
+	// 		case <-stop:
+	// 			// ticker.Stop()
+	// 			interrupt()
+	// 			return
+	// 		case <-ticker.C:
+	// 			timeLeft--
+	// 			callback(timeLeft)
+	// 		}
+	// 	}
+	// }()
+
+	for {
+		select {
+		case <-stop:
+			// ticker.Stop()
+			interrupt()
+			return
+		case <-ticker.C:
+			timeLeft--
+			callback(timeLeft)
+
+			if timeLeft <= 0 {
+				end()
+				return
+			}
+		}
+	}
+
+	// time.Sleep(sleep * time.Second)
+	// ticker.Stop()
+	// end()
+}
+
+// func example() {
+// 	Timer(3, func(duration int) {
+// 		fmt.Printf("Time: %d\n", duration)
+// 	}, func() {
+// 		fmt.Println("Ticker stopped.")
+// 	})
+// }
