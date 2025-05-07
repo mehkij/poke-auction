f532aaef167b244b87e47ba0b81df1a609aefa9a
mehkij <disismehki@gmail.com>
2025-05-07 07:28:34 -0400

    fix: exporting team no longer creates file in filesystem

--- internal/export/team.go
+++ internal/export/team.go
@@ -3,8 +3,6 @@ package export
 import (
 	"bytes"
 	"fmt"
-	"log"
-	"os"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/mehkij/poke-auction/internal/types"
@@ -19,16 +17,10 @@ func ExportTeam(s *discordgo.Session, i *discordgo.InteractionCreate, players []
 			d = fmt.Appendf(d, "%s\n\n", pokemon.Name)
 		}
 
-		filename, data, err := WriteToFile(d, player.Username)
-		if err != nil {
-			log.Printf("an error occurred while trying to export team: %v", err)
-			return
-		}
-
 		files = append(files, &discordgo.File{
-			Name:        filename,
+			Name:        fmt.Sprintf("%s_team.txt", player.Username),
 			ContentType: "text/plain",
-			Reader:      bytes.NewReader(data),
+			Reader:      bytes.NewReader(d),
 		})
 	}
 
@@ -38,22 +30,4 @@ func ExportTeam(s *discordgo.Session, i *discordgo.InteractionCreate, players []
 	}
 
 	s.ChannelMessageSendComplex(i.ChannelID, msg)
-}
-
-func WriteToFile(data []byte, username string) (string, []byte, error) {
-	teamsDir := "teams"
-	if err := os.MkdirAll(teamsDir, 0755); err != nil {
-		return "", nil, fmt.Errorf("failed to create teams directory: %w", err)
-	}
-
-	filename := fmt.Sprintf("%s_team.txt", username)
-	filepath := fmt.Sprintf("%s/%s", teamsDir, filename)
-
-	err := os.WriteFile(filepath, data, 0644)
-	if err != nil {
-		return "", nil, err
-	}
-
-	log.Printf("file successfully created: %s_team.txt", username)
-	return filename, data, nil
 }
