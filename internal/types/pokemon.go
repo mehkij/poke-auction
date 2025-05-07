754abdc5deae531c77090735545ca26050e150f8
mehkij <disismehki@gmail.com>
2025-05-07 07:15:19 -0400

    fix: simplify the exported text file's contents
    The randbat data cannot be used properly to create teams, as the data is mismatched from what is allowed
    in all Pokemon Showdown formats, causing you to have to manually fix the data. So instead, we can just print
    a list of Pokemon names and use that to import teams instead, and manually add the data we want for our teams.

--- internal/types/pokemon.go
+++ internal/types/pokemon.go
@@ -1,26 +1,11 @@
 package types
 
-type StatMap map[string]int
-
-type Role struct {
-	Abilities []string `json:"abilities,omitempty"`
-	Items     []string `json:"items,omitempty"`
-	Moves     []string `json:"moves"`
-	TeraTypes []string `json:"teratypes,omitempty"`
-	IVs       StatMap  `json:"ivs,omitempty"`
-}
-
-type Pokemon struct {
-	Name      string          `json:"name"`
-	Level     int             `json:"level"`
-	Abilities []string        `json:"abilities,omitempty"`
-	Items     []string        `json:"items,omitempty"`
-	Moves     []string        `json:"moves,omitempty"`
-	Roles     map[string]Role `json:"roles,omitempty"`
-	IVs       StatMap         `json:"ivs,omitempty"`
-
-	Sprite string
-}
-
 // PokemonMap represents the top-level JSON structure where pokemon name is the key
 type PokemonMap map[string]*Pokemon
+
+type StatMap map[string]int
+
+type Pokemon struct {
+	Name   string `json:"name"`
+	Sprite string
+}
