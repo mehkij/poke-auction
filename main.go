72bb5d9b80d798e021c21f4bd46f124ef0e4512b
mehkij <disismehki@gmail.com>
2025-05-17 05:21:04 -0400

    fix: enable CORS

--- main.go
+++ main.go
@@ -53,6 +53,15 @@ func main() {
 		})
 
 		http.HandleFunc("/api/status", func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+			if r.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+
 			w.Header().Set("Content-Type", "application/json")
 
 			guilds := len(session.State.Guilds)
