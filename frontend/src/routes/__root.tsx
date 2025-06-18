535259de96f0e5b320e53f35b4448c02a6c53756
mehkij <disismehki@gmail.com>
2025-06-18 12:16:38 -0400

    feat: add view transitions

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -2,24 +2,33 @@ import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
-  component: () => (
+  component: RootComponent
+});
+
+function RootComponent() {
+  return (
     <>
       <nav className="p-4 flex gap-4">
         <Link
           to="/"
           className="[&.active]:bg-gray-400/25 rounded p-2 transition-all"
+          viewTransition
         >
           Home
         </Link>{" "}
         <Link
           to="/changelog"
           className="[&.active]:bg-gray-400/25 rounded p-2 transition-all"
+          viewTransition
         >
           Changelog
         </Link>
       </nav>
-      <Outlet />
+
+      <div className="transition-opacity">
+        <Outlet />
+      </div>
       <TanStackRouterDevtools />
     </>
-  ),
-});
+  )
+}
