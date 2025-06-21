030fc61f6053460ccd25a0edc2573bb7cdad4b6c
mehkij <disismehki@gmail.com>
2025-06-21 16:08:59 -0400

    fix: typo

--- frontend/src/changelog.ts
+++ frontend/src/changelog.ts
@@ -9,7 +9,7 @@ export const changelog: ChangelogEntry[] = [
   {
     version: "1.1.1",
     date: "2025-06-21",
-    description: "A quick fix to an annoying...",
+    description: "A quick fix to an annoying bug...",
     changes: [
       `Fixed an issue where if you force start an auction with no players, and then you begin a successful auction, you get an "application did not respond" error after attempting to nominate a Pokemon`,
     ],
