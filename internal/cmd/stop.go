175823d03c24abb5ad7847676a14733608690fcf
mehkij <disismehki@gmail.com>
2025-06-22 20:11:10 -0400

    fix: closing channel panic
    Closes #4
    Closes #5

--- internal/cmd/stop.go
+++ internal/cmd/stop.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/mehkij/poke-auction/internal/dispatcher"
+	"github.com/mehkij/poke-auction/internal/utils"
 )
 
 func StopAllCallback(s *discordgo.Session, i *discordgo.InteractionCreate, gd *dispatcher.Dispatcher) {
@@ -25,13 +26,24 @@ func StopAllCallback(s *discordgo.Session, i *discordgo.InteractionCreate, gd *d
 		if state.ChannelID == i.ChannelID {
 			channelID = state.ChannelID
 
+			state.AuctionStateMu.Lock()
+
+			log.Printf("Attempting to stop auction for message %s in channel %s", msgID, state.ChannelID)
+
+			// Try to fetch the message before editing
+			_, err := s.ChannelMessage(state.ChannelID, msgID)
+			if err != nil {
+				log.Printf("Could not fetch message %s: %v", msgID, err)
+			} else {
+				log.Printf("Fetched message %s successfully", msgID)
+			}
+
 			log.Println("Interrupting timers...")
 			// Interrupt the auction's timer if it is running
-			state.StopSignal <- true
-			close(state.StopSignal)
+			utils.SafeCloseChannel(state.StopSignal)
 			log.Println("Timers interrupted.")
 
-			gd.QueueEditMessage(s, state.ChannelID, msgID, &discordgo.MessageEdit{
+			done := gd.QueueEditMessage(s, state.ChannelID, msgID, &discordgo.MessageEdit{
 				Channel: state.ChannelID,
 				ID:      msgID,
 				Embeds: &[]*discordgo.MessageEmbed{
@@ -43,10 +55,19 @@ func StopAllCallback(s *discordgo.Session, i *discordgo.InteractionCreate, gd *d
 				Components: &[]discordgo.MessageComponent{},
 			})
 
+			editedMsg := <-done
+			if editedMsg == nil {
+				log.Printf("Failed to edit message %s in channel %s", msgID, state.ChannelID)
+			} else {
+				log.Printf("Successfully edited message %s in channel %s", msgID, state.ChannelID)
+			}
+
 			log.Println("Deleting auction state...")
 			// Delete the auction's state from the channel
 			delete(auctionStates, msgID)
 			log.Println("Auction state deleted.")
+
+			state.AuctionStateMu.Unlock()
 		}
 	}
 	mu.Unlock()
